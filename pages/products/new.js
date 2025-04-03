e545c0f4c146b7c662e140fd5b479707db7a8e4d
JoAnna Scott-Scates <joanna.scott.uky@gmail.com>
2025-04-03 16:45:02 -0400

    Added a modal to alert user to price error if it exceeded the maximum allowed price.

--- pages/products/new.js
+++ pages/products/new.js
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router'
-import { useRef } from 'react'
+import { useRef, useState } from 'react'
 import Layout from '../../components/layout'
 import Navbar from '../../components/navbar'
 import { addProduct } from '../../data/products'
 import ProductForm from '../../components/product/form'
+import Modal from '../../components/modal'
 export default function NewProduct() {
   const formEl = useRef()
   const router = useRouter()
+  const [showModal, setShowModal] = useState(false)
 
   const saveProduct = () => {
     const { name, description, price, category, location, quantity  } = formEl.current
@@ -14,20 +16,34 @@ export default function NewProduct() {
       name: name.value,
       description: description.value,
       price: price.value,
-      categoryId: category.value,
+      category_id: category.value,
       location: location.value,
       quantity: quantity.value
     }
-    addProduct(product).then((res) => router.push(`/products/${res.id}`))
-  }
+    addProduct(product).then((res) => {
+      if (!res?.id){
+        setShowModal(true);
+      } else {
+        router.push(`/products/${res.id}`)
+      }
 
+    })
+
+  }
+console.log(showModal)
   return (
+    <>
+     <Modal showModal={showModal} setShowModal={setShowModal} title="Price is out of range.">
+      <div className="modal-card-body">Your price is over the allowed $17,500. Please modify and try again.</div>
+      <footer></footer>
+     </Modal>
     <ProductForm
       formEl={formEl}
       saveEvent={saveProduct}
       title="Add a new product"
       router={router}
     ></ProductForm>
+    </>
   )
 }
 
