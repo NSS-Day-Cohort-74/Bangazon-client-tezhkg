cd8b2e5afd0e5ca19db77297c62f9dd9c35027fb
Sabra Kozlina <sjkozlina@gmail.com>
2025-04-16 15:01:35 -0600

    fixed random bugs

--- components/filter.js
+++ components/filter.js
@@ -190,6 +190,7 @@ export default function Filter({ productCount, onSearch, locations, setSearching
                             }
                           }}
                         />
+                        {" "}Descending
                       </label>
                     </div>
                   </div>
